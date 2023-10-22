c2ae1b6960a26eb0f2270388d1fd53b2c28257e0
rsliaprjs <rsliaprjs@gmail.com>
2023-10-22 18:36:44 +0300

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const App = () => {
+  return (
+    <div className="w-full flex flex-col items-center gap-8 p-8">
+      <h1 className="text-red-500 text-3xl">Heading</h1>
+      <button className="btn">Button</button>
+    </div>
+  );
+};
+
+export default App;
